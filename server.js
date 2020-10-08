f01d78ce9d7b5cb5b14a2d4822032c6a3e1ae7c4
osamudiamen <osas.azamegbe@gmail.com>
2020-10-08 22:13:11 +0100

    created Auth route

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const authRoute = require('./routes/Auth')
 
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
+//MIDDLEWARE
+app.use('/auth', authRoute)
+
+// ROUTES
 app.get('/', (req, res) => {
     res.send("App has started. Welcome to the home page.")
 })
