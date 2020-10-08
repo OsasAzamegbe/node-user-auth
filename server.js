7d9fea5c6aebd6671e5fed683494a6feab192d18
osamudiamen <osas.azamegbe@gmail.com>
2020-10-08 22:39:07 +0100

    set up database connection to mongodb atlas

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const apiRoute = require('./routes/Api')
 
+require('dotenv').config()
 
+
+//DB
+const DB_NAME = process.env.DB_NAME
+const DB_USERNAME = process.env.DB_USERNAME
+const DB_PASSWORD = process.env.DB_PASSWORD
+
+mongoose.connect(`mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.yttoy.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
+{ 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}, 
+() => {console.log('connected to database')})
+
+
+//APP
 const app = express()
 const PORT = process.env.PORT || 5000
 
