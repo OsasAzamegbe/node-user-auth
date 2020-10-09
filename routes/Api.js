f0eba838d23bd998fb76655e037c9ffc2b0ebcc3
osamudiamen <osas.azamegbe@gmail.com>
2020-10-09 18:31:08 +0100

    added verifyToken middleware

--- routes/Api.js
+++ routes/Api.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const authRoute = require('./Auth')
 const postsRoute = require('./Posts')
+const verify = require('../validation/verifyToken')
 
 
 const router = express.Router()
 
 //API MIDDLEWARE
 router.use('/auth', authRoute)
-router.use('/posts', postsRoute)
+router.use('/posts', verify, postsRoute)
 
 module.exports = router
--- validation/verifyToken.js
+++ validation/verifyToken.js
@@ -5,8 +5,11 @@ const verifyToken = (req, res, next) => {
         const token = req.header("Authorization")
         const verified = jwt.verify(token, process.env.JWT_TOKEN_SECRET)
         req.user = verified
+        next()
     } catch (err) {
         res.status(401).json({"error": "Access Denied"})
     }
     
 }
+
+module.exports = verifyToken
