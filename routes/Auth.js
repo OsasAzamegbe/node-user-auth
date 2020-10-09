18d8748f501ee76c0f6d698f60439dafa99872df
osamudiamen <osas.azamegbe@gmail.com>
2020-10-09 12:18:44 +0100

    encrypted password

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
 const User = require('../models/User')
 const {registerValidation} = require('../validation/Validation')
+
 
 
 const router = express.Router()
@@ -21,20 +23,27 @@ router.post('/register', async (req, res) => {
     }
 
     //check if email is unique
-    const emailExists = User.findOne({email: body.email})
+    const emailExists = await User.findOne({email: body.email})
     if(emailExists){
         return res.status(400).json({"error": "An account with this email address already exists."})
     }
 
     try{
+        //encrypt password
+        const salt = await bcrypt.genSalt(10)
+        const encryptedPassword = await bcrypt.hash(body.password, salt)
+
         const user = new User({
             username: body.username,
             email: body.email,
-            password: body.password
+            password: encryptedPassword
         })
 
         const newUser = await user.save()
-        res.status(201).json({newUser})
+        res.status(201).json({
+            userCreated: true,
+            newUserId: newUser._id
+        })
 
     } catch (error) {
         res.status(500).json({error})
