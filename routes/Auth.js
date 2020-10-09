4218d7c4ab9871d97167e0bcc700e080db8b26ac
osamudiamen <osas.azamegbe@gmail.com>
2020-10-09 11:01:14 +0100

    added validation for register and login with @hapi/joi

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/User')
+const {registerValidation} = require('../validation/Validation')
 
 
 const router = express.Router()
@@ -10,6 +11,13 @@ router.get('/', (req, res) => {
 
 // REGISTER NEW USER
 router.post('/register', async (req, res) => {
+
+    const {error} = registerValidation(req.body)
+
+    if (error) {
+        return res.status(400).json({"validation error" : error.details[0].message})
+    }
+
     try{
         const body = req.body
         const user = new User({
---
+++ validation/Validation.js
@@ -0,0 +1,25 @@
+const joi = require('@hapi/joi')
+
+
+// REGISTER VALIDATION
+const registerValidation = (body) => {
+    const schema = joi.object({
+        username: joi.string().max(200).required(),
+        email: joi.string().max(200).required().email(),
+        password: joi.string().max(2048).min(8).required(),
+    })
+
+    return schema.validate(body)
+}
+
+// LOGIN VALIDATION
+const loginValidation = (body) => {
+    const schema = joi.object({
+        username: joi.string().max(200).required(),
+        password: joi.string().max(2048).min(8).required(),
+    })
+
+    return schema.validate(body)
+}
+
+module.exports = {registerValidation, loginValidation}
