c8457f552a5781b6e1586054532dc28918ca8f21
osamudiamen <osas.azamegbe@gmail.com>
2020-10-09 12:43:34 +0100

    installed jsonwebtoken

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const User = require('../models/User')
 const {registerValidation, loginValidation} = require('../validation/Validation')
 
